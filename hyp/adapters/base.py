2006fa8a5060b62ebd5ec314e71423a9ed7a61d2
Kevin Qiu <kevin@freshbooks.com>
2014-06-25 23:09:17 -0400

    Defer importing concrete adaptor until runtime.

--- hyp/adapters/base.py
+++ hyp/adapters/base.py
@@ -1,13 +1,12 @@
 from __future__ import absolute_import
 
-from schematics.models import Model as SchematicsSerializer
-from marshmallow import Serializer as MarshmallowSerializer
-
-from hyp.adapters.schematics import Adapter as SchematicsAdapter
-from hyp.adapters.marshmallow import Adapter as MarshmallowAdapter
-
-
 def adapter_for(serializer):
+    from schematics.models import Model as SchematicsSerializer
+    from marshmallow import Serializer as MarshmallowSerializer
+
+    from hyp.adapters.schematics import Adapter as SchematicsAdapter
+    from hyp.adapters.marshmallow import Adapter as MarshmallowAdapter
+
     if isinstance(serializer(), SchematicsSerializer):
         return SchematicsAdapter
     elif isinstance(serializer(), MarshmallowSerializer):
